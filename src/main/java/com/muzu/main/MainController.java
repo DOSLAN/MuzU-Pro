0bfdc2c9d4092ab1fc4581d3be1988f70f397353
DOSLAN <doslan130400@gmail.com>
2021-01-03 22:24:08 +0600

    first commit

---
+++ src/main/java/com/muzu/main/MainController.java
@@ -0,0 +1,25 @@
+package com.muzu.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+
+    @FXML
+    private Button leftBtn;
+
+    @FXML
+    private SplitPane mainSplitPane;
+
+    @FXML
+    private AnchorPane midiAnchor;
+
+    @FXML
+    private AnchorPane musicAnchor;
+
+    @FXML
+    private Button rightBtn;
+
+}
